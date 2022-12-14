a4cb257dc4cee73220c0bb7fd6c03e066c72983f
mine9607 <81383389+mine9607@users.noreply.github.com>
2022-12-14 12:13:57 -0500

    added js file

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
+
+const para = document.createElement('p');
+para.classList.add('para');
+para.textContent = "Hey I'm red!";
+
+container.appendChild(para);
